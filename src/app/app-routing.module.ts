44819b3b801671cc1580587d1c8c4a83067b1643
sinan-marayi <sinanmarayi8@gmail.com>
2022-11-09 12:02:35 +0530

    manage page basic setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,20 +5,38 @@ import { CategoryComponent } from './components/category/category.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { ManageComponent } from './components/manage/manage.component';
+import { EditCarouselComponent } from './components/managePages/edit-carousel/edit-carousel.component';
+import { EditCategoryComponent } from './components/managePages/edit-category/edit-category.component';
+import { EditProductComponent } from './components/managePages/edit-product/edit-product.component';
+import { NewCarouselComponent } from './components/managePages/new-carousel/new-carousel.component';
+import { NewCategoryComponent } from './components/managePages/new-category/new-category.component';
+import { NewProductComponent } from './components/managePages/new-product/new-product.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
-{path:'',component:HomeComponent},
-{path:'login',component:LoginComponent},
-{path:'register',component:RegisterComponent},
-{path:'category',component:CategoryComponent},
-{path:'cart',component:CartComponent,canActivate:[AuthGuard]},
-{path:'manage',component:ManageComponent,canActivate:[AuthGuard]},
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'category', component: CategoryComponent },
+  { path: 'cart', component: CartComponent, canActivate: [AuthGuard] },
+  {
+    path: 'manage',
+    component: ManageComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'newProduct', component: NewProductComponent },
+      { path: 'editProduct', component: EditProductComponent },
+      { path: 'newCarousel', component: NewCarouselComponent },
+      { path: 'editCarousel', component: EditCarouselComponent },
+      { path: 'newCategory', component: NewCategoryComponent },
+      { path: 'editCategory', component: EditCategoryComponent },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
